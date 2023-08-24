2828c34bd9a55013d83f8590681b750fcfeab538
zenonsenn <zenonsenn@gmail.com>
2023-08-24 09:27:37 +0700

    create pf
    temp

---
+++ pf/pages/index.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+export default function Home() {
+    const [backendData, setBackendData] = useState("");
+    const backendUrl = process.env.BACKEND_URL || "http://localhost:3080/api/test";
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(backendUrl); // Change to your backend URL
+                const data = await response.json();
+                setBackendData(data.message);
+            } catch (error) {
+                console.error("Error fetching data from backend:", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>hello website!</h1>
+            <p>test paragraph</p>
+            <p>and a message from our backend: "{backendData}"</p>
+        </div>
+    );
+}
